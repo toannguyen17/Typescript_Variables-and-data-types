acf348bbe2b53103fbcc000c50ac5fd28d51c680
MrT98 <toan.vp98.wapmaster@gmail.com>
2020-09-22 10:34:23 +0700

    first commit

---
+++ dist/fundamentals.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("./scss/styles.scss");
+function main() { }
+main();
+//# sourceMappingURL=fundamentals.js.map
---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("./scss/styles.scss");
+console.log("it's work!");
+//# sourceMappingURL=index.js.map
